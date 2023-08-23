473d6c579d4a81bd9dff91ecba48a5c255116c30
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-23 13:39:35 +0200

    Implementazione caricamento file DB

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -23,6 +23,7 @@ class PostSeeder extends Seeder
             $post->title = $faker->sentence(3);
             $post->content = $faker->text(500);
             $post->slug = $post->generateSlug($post->title);
+            $post->image = $faker->imageUrl(640, 480, 'animals', true);
 
             $post->save();
         }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,13 +8,21 @@
                 <a href="{{ route('admin.posts.index') }}" class="btn btn-sm btn-primary">Ritorna alla lista completa</a>
             </div>
             <div class="col-12">
-                <form action="{{ route('admin.posts.store') }}" method="POST">
+                <form action="{{ route('admin.posts.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group mt-4">
                         <label class="contol-lable">Titolo</label>
                         <input class="form-control @error('title')is-invalid @enderror" type="text" name="title"
                             id="title" placeholder="Titolo" value="{{ old('title') }}">
                         @error('title')
+                            <div class="text-danger">{{ $messages }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group mt-4">
+                        <label class="contol-lable">Immagine</label>
+                        <input class="form-control @error('image')is-invalid @enderror" type="file" name="image"
+                            id="image">
+                        @error('image')
                             <div class="text-danger">{{ $messages }}</div>
                         @enderror
                     </div>
